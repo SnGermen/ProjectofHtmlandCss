e127badf0b50093d0def0bf741fe9cfe438242ea
SnGermen <144361057+SnGermen@users.noreply.github.com>
2024-03-25 18:20:41 +0100

    popUp

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 (() => {
   let popUp = document.querySelector('.popUp')
 
-  document.querySelector('.open__popUp').onclick = function () {
+  document.querySelector('.button_primery').onclick = function () {
     popUp.style.display = 'block'
   }
 
-  document.querySelector('.header__mobile-menu-buttun').onclick = function () {
+  document.querySelector('.popUp__form-button').onclick = function () {
     popUp.style.display = 'none'
   }
 })()
