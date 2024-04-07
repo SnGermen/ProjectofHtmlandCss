e23341e59e263252d60a3a30b16970e1416e925d
SnGermen <144361057+SnGermen@users.noreply.github.com>
2024-04-07 03:41:33 +0200

    я рот БЕМА в рот *****

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 (() => {
   let popUp = document.querySelector('.popUp')
 
-  document.querySelector('.button--primary').onclick = function () {
+  document.querySelector('.button_primary').onclick = function () {
     popUp.style.display = 'block'
   }
 
-  document.querySelector('.popUp__form-button').onclick = function () {
+  document.querySelector('.popUp__button').onclick = function () {
     popUp.style.display = 'none'
   }
 })()
